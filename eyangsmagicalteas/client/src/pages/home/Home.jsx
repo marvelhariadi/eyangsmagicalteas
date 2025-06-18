45325aeb9c9ef246b76bcbf288cac0e0359a20a2
marvelhariadi <marvelhariadi@gmail.com>
2025-06-17 22:56:23 -0700

    stytle

--- eyangsmagicalteas/client/src/pages/home/Home.jsx
+++ eyangsmagicalteas/client/src/pages/home/Home.jsx
@@ -1,11 +1,18 @@
 import { Category, Hero, Offer, Product, Support } from "../../utils/Route";
-
+import { CategoryHomeShowcase } from "../../components/home/CategoryHomeShowcase.jsx";
 
 export const Home = () => {
   return (
     <>
       <Hero />
       <Category />
+      <CategoryHomeShowcase 
+        sectionTitle="Our Best Sellers"
+        sectionDescription="Our most popular and highly-rated magical teas."
+        categorySlug="best-sellers"
+        numItemsToDisplay={3}
+        randomizeDisplay={true}
+      />
       <Product />
     </>
   );
